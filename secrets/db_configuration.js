dc139f0fd9825241c49952a86bdd7aa2d3ca5cc3
Trung Truc Nguyen <trungtrucnguyen@Trungs-MacBook-Pro-2.local>
2018-05-07 01:37:34 +1200

    second commit

--- secrets/db_configuration.js
+++ secrets/db_configuration.js
@@ -1,7 +1,7 @@
 module.exports = {
-    user: 'node_user',
-    password: 'node_password',
-    host: 'localhost',
-    database: 'monstersdb',
+    user: 'euvasfqcpgvahm',
+    password: '24a0d08edf3dbee4d841f9a6f861dc30221c19678833f7301f8d50d7d3dce216',
+    host: 'ec2-54-243-54-6.compute-1.amazonaws.com',
+    database: 'd428pf7hjrpdsr',
     port: 5432
 }
