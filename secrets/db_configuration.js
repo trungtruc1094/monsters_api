ad0d946cc99c651495f8a898ffcdaec8e256a245
Trung Truc Nguyen <trungtrucnguyen@Trungs-MacBook-Pro-2.local>
2018-05-07 02:26:07 +1200

    abc

--- secrets/db_configuration.js
+++ secrets/db_configuration.js
@@ -4,6 +4,7 @@ module.exports = {
         password: '24a0d08edf3dbee4d841f9a6f861dc30221c19678833f7301f8d50d7d3dce216',
         host: 'ec2-54-243-54-6.compute-1.amazonaws.com',
         database: 'd428pf7hjrpdsr',
+        ssl: true,
         port: 5432
     },
     db_local: {
